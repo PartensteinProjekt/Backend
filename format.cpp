134fc3ed7b4063b6035297647ea539ea431dd659
JMaster <chadwick.holderbuckle@byom.de>
2017-07-29 13:24:43 +0200

    schönheitskorrekturen

--- format.cpp
+++ format.cpp
@@ -1,28 +1,29 @@
-// --------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------
 #include <format.h>
-// --------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------
 #include <string>
+using std::string;
 using std::to_string;
 
 // --------------------------------------------------------------------------------
-std::string to_string(const Arg& arg)
+string to_string(const Arg& arg)
 // --------------------------------------------------------------------------------
 {
     auto index = arg.index();
     if (index == 0) {
-        return std::to_string(std::get<int_index>(arg));
+        return to_string(std::get<int_index>(arg));
     }
     if (index == 1) {
-        return std::to_string(std::get<double_index>(arg));
+        return to_string(std::get<double_index>(arg));
     }
     return std::get<string_index>(arg);
 }
 
 // --------------------------------------------------------------------------------
-std::string format(const char* fmt, const Arg* values, size_t values_size)
+string format(const char* fmt, const Arg* values, size_t values_size)
 // --------------------------------------------------------------------------------
 {
-    std::string out;
+    string out;
     while (*fmt) {
         if (*fmt == '%') {
             fmt++;
