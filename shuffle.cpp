1eaca9f271a38c11d20a4d68a3b5573399f8f001
JMaster <chadwick.holderbuckle@byom.de>
2017-08-02 21:41:40 +0200

    Einfacher balancierter Shuffle Algo

---
+++ shuffle.cpp
@@ -0,0 +1,68 @@
+// --------------------------------------------------------------------------
+#include "shuffle.h"
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+albums shuffle::balanced_shuffle(albums original_playlist)
+// --------------------------------------------------------------------------
+{
+    // 0 : edge case handling
+    // ... for no elements
+    if (original_playlist.empty()) {
+        return {};
+    }
+    // ... for only one album
+    if (original_playlist.size() == 1) {
+        simple_album_shuffle(original_playlist[0]);
+        return albums{ original_playlist };
+    }
+
+    // 1 : shuffle each album
+    simple_playlist_shuffle(original_playlist);
+
+    // 2 : find the largest album
+    const auto max_size = std::max_element(std::begin(original_playlist), std::end(original_playlist), [](const album& a, const album& b) {
+        return a.size() < b.size();
+    })->size();
+
+    // 3 : bring all albums to the same length by inserting dummy tracks
+    for (auto& album : original_playlist) {
+        while (album.size() < max_size) {
+            album.push_back({/*dummy!*/});
+        }
+    }
+
+    // 4 : shuffle all albums again
+    simple_playlist_shuffle(original_playlist);
+
+    // 5 : column select the virtual playlist table
+    album balanced_list {};
+    for (size_t n = 0; n < max_size; ++n) {
+        for (auto album : original_playlist) {
+            const auto track = album[n];
+            if (track) {
+                balanced_list.push_back(track);
+            }
+        }
+    }
+
+    return albums{ balanced_list };
+}
+
+// --------------------------------------------------------------------------
+void shuffle::simple_album_shuffle(album& single_album)
+// --------------------------------------------------------------------------
+{
+    auto random_engine = std::default_random_engine{};
+    std::shuffle(std::begin(single_album), std::end(single_album), random_engine);
+}
+
+// --------------------------------------------------------------------------
+void shuffle::simple_playlist_shuffle(albums& album_list)
+// --------------------------------------------------------------------------
+{
+    auto random_engine = std::default_random_engine{};
+    for (auto& single_album : album_list) {
+        std::shuffle(std::begin(single_album), std::end(single_album), random_engine);
+    }
+}
---
+++ shuffle.h
@@ -0,0 +1,51 @@
+// --------------------------------------------------------------------------
+// functions to shuffle playlists ~ JSt
+// --------------------------------------------------------------------------
+#pragma once
+
+#include <random>    // for random_engine
+#include <stdint.h>  // for int64_t
+#include <algorithm> // for std::shuffle
+#include <vector>    // for std::vector
+#include <optional>  // for std::optional
+
+// demo struct for testing... create a real struct for tracks ASAP!
+//struct track {
+// int64_t track_id;
+// int64_t album_id;
+// int64_t artist_id;
+//};
+using track = std::string;
+using album = std::vector<std::optional<track>>;
+using albums = std::vector<album>;
+// end demo structs
+
+
+
+namespace shuffle {
+
+using std::vector;
+
+
+// -----------------------------------------------------------------
+// shuffle a playlist by balancing ~ JSt
+// -----------------------------------------------------------------
+// concept for balanced_shuffle:
+// - sort each album              | 1
+// - find the largest album       | 2
+// - insert dummys to spread all  | 3
+//   other albums                 |
+// - shuffle all albums again     | 4
+// - all songs are now a table    |
+//   with same length columns;    |
+//   get songs from every column  | 5
+// -----------------------------------------------------------------
+albums balanced_shuffle(albums playlist);
+// -----------------------------------------------------------------
+
+// -----------------------------------------------------------------
+// shuffle track by track with std algorithms ~ JSt
+// -----------------------------------------------------------------
+void simple_album_shuffle(album& single_album);
+void simple_playlist_shuffle(albums& album_list);
+}
